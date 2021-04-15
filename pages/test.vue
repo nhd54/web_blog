57331067969e65d669c8a526f40b624451c9805f
nhd54 <nicklasdegnebolig@gmail.com>
2021-04-15 20:51:44 +0200

    first commit

---
+++ pages/test.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+    <!-- Header Start -->
+    <header>
+      <div>Logo</div>
+      <nav>
+        <ul>
+          <li><a href="#">Forside</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Kontakt</a></li>
+        </ul>
+      </nav>
+    </header>
+    <!-- Header end -->
+    <!-- welcome section Start -->
+    <section>
+      <article>
+        <span>Navn</span>
+        <h1>Web Udvikler</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+          eum voluptas. Officiis libero iure nemo debitis repellendus asperiores
+          veritatis sunt neque. Possimus hic alias iusto ratione sit facilis
+          nobis praesentium.
+        </p>
+      </article>
+      <div>Nyhedsbrev (card)</div>
+    </section>
+    <!-- welcome section end -->
+    <!-- Blog start -->
+    <section>
+      <article>
+        <span>Navn</span>
+        <h1>Web Udvikler</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+          voluptatum quam reiciendis doloremque omnis molestiae, eos repellendus
+          aperiam vero delectus cum. Commodi praesentium ipsa illum atque
+          molestiae assumenda vel ducimus.
+        </p>
+        <a href="">Læs mere!</a>
+      </article>
+      <div>
+        <article>
+          <img src="" alt="" />
+          <div>
+            <h2>Blog Post</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error,
+              veniam deleniti, voluptate, ducimus aliquid quae exercitationem
+              eum libero ex iure nam blanditiis atque harum molestiae corporis
+              reiciendis ut quia? Neque.
+            </p>
+            <span>Nicklas Degnebolig - d.03.04.2021</span>
+            <div>Badges</div>
+          </div>
+        </article>
+
+        <article>
+          <img src="" alt="" />
+          <div>
+            <h2>Blog Post</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error,
+              veniam deleniti, voluptate, ducimus aliquid quae exercitationem
+              eum libero ex iure nam blanditiis atque harum molestiae corporis
+              reiciendis ut quia? Neque.
+            </p>
+            <span>Nicklas Degnebolig - d.03.04.2021</span>
+            <div>Badges</div>
+          </div>
+        </article>
+
+        <article>
+          <img src="" alt="" />
+          <div>
+            <h2>Blog Post</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error,
+              veniam deleniti, voluptate, ducimus aliquid quae exercitationem
+              eum libero ex iure nam blanditiis atque harum molestiae corporis
+              reiciendis ut quia? Neque.
+            </p>
+            <span>Nicklas Degnebolig - d.03.04.2021</span>
+            <div>Badges</div>
+          </div>
+        </article>
+      </div>
+    </section>
+    <!-- Blog end -->
+    <footer>
+      <nav>
+        <ul>
+          <li><a href="#">Forside</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Kontakt</a></li>
+        </ul>
+      </nav>
+      <div>
+        <ul>
+          <li><span>Facebook</span></li>
+          <li><span>Linkedin</span></li>
+          <li><span>Twitter</span></li>
+          <li><span>Github</span></li>
+        </ul>
+      </div>
+      <div>&copy; 2021 - Nicklas Hvid Degnebolig. All rights reserved</div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
