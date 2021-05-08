c68a7b82b52b6ef6a56d7190d1fa7b58d0f1be98
nhd54 <nicklasdegnebolig@gmail.com>
2021-05-08 14:31:32 +0200

    first skelet

---
+++ components/Teaser.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-editable="blok" class="py-8 mb-6 text-5xl font-bold text-center">
+    <img
+      :src="blok.image.filename"
+      class="h-48 w-full mb-4 object-cover"
+      alt=""
+    />
+    {{ blok.headline }}
+    <section v-for="blok in blok.columns" :key="blok.uid">
+      <component :blok="blok" :is="blok.component" />
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true,
+      default: '',
+    },
+  },
+  mounted() {
+    console.log('teaser: ', this.$props.blok.columns[0])
+  },
+}
+</script>
